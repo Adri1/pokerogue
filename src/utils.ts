7e2633991837bbcdb4f865e17ff057de865d5f3e
Lugiad <2070109+Adri1@users.noreply.github.com>
2025-01-21 17:32:01 +0100

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -462,6 +462,7 @@ export function hasAllLocalizedSprites(lang?: string): boolean {
     case "pt-BR":
     case "ko":
     case "ja":
+    case "sl":
       return true;
     default:
       return false;
